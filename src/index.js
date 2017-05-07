a499aefad393e8c063586d2d348db009de590709
Leslie Robertson <leslievrobertson@gmail.com>
2017-05-07 12:48:44 -0500

    Final Commit

--- src/index.js
+++ src/index.js
@@ -2,35 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import state from "./state";
+// import state from "./state";
 
 import store from "./store";
 import {Provider} from "react-redux";
 
-const {
-dateTime,
-newComments,
-newTasks,
-newOrders,
-tickets,
-orders,
-taskItem,
-tasks,
-messages
-} = state;
+// const {
+// dateTime,
+// newComments,
+// newTasks,
+// newOrders,
+// tickets,
+// orders,
+// taskItem,
+// tasks,
+// messages
+// } = state;
 
 ReactDOM.render(
   <Provider store={store}>
     <App
-      taskItem={taskItem}
-      dateTime={dateTime}
-      newComments={newComments}
-      newTasks={newTasks}
-      newOrders={newOrders}
-      tickets={tickets}
-      orders={orders}
-      messages={messages}
-      tasks={tasks}
+      // taskItem={taskItem}
+      // dateTime={dateTime}
+      // newComments={newComments}
+      // newTasks={newTasks}
+      // newOrders={newOrders}
+      // tickets={tickets}
+      // orders={orders}
+      // messages={messages}
+      // tasks={tasks}
     />
   </Provider>,document.getElementById("root")
 );
