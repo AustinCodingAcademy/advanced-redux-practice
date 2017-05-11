6c33f320fc84ed6781564e7a91f18d2b52c4b642
RX7RUSS <russ.savage5@gmail.com>
2017-05-11 05:25:36 -0500

    All reducers added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,9 +8,45 @@ function tasks(state = []) {
   return state;
 }
 
+function dateTime(state = []) {
+  return state;
+}
+
+function newComments(state = []) {
+  return state;
+}
+
+
+function newTasks(state = []) {
+  return state;
+}
+
+function newOrders(state = []) {
+  return state;
+}
+
+function tickets(state = []) {
+  return state;
+}
+
+function orders(state = []) {
+  return state;
+}
+
+function taskItem(state = []) {
+  return state;
+}
+
 const rootReducer = combineReducers({
   messages,
-  tasks
+  tasks,
+  dateTime,
+  newComments,
+  newTasks,
+  newOrders,
+  tickets,
+  orders,
+  taskItem,
 });
 
 export default rootReducer;
--- src/store.js
+++ src/store.js
@@ -18,10 +18,17 @@ const {
 export default createStore(
   reducer,
   {
+    dateTime,
+    newComments,
+    newTasks,
+    newOrders,
+    tickets,
+    orders,
+    taskItem,
     tasks,
     messages
   },
-
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 // export default store;
