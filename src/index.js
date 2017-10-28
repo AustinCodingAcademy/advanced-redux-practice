8690b7768c80602da7ba5e2a27a7518119dc3ab9
Jordon R <reese.jordon@gmail.com>
2017-10-28 15:00:01 -0500

    Create reducers folder and index.js along with reducer functions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import state from "./state";
+import {Provider} from 'react-redux';
+
+function renderApp() {
+  render(<Provider store={store}><App /><Provider>,
+  document.getElementById('root'));
+}
+rederApp();
 
 const {
 dateTime,
@@ -17,14 +24,14 @@ messages
 } = state;
 
 ReactDOM.render(
-  <App 
+  <App
     taskItem={taskItem}
     dateTime={dateTime}
     newComments={newComments}
     newTasks={newTasks}
     newOrders={newOrders}
     tickets={tickets}
-    orders={orders} 
+    orders={orders}
     messages={messages}
     tasks={tasks}
     />,
