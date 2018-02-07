f57d37be6c9cfb5f38a4ca19a369d3a37b0b29d6
unknown <geohalbert@gmail.com>
2018-02-06 22:40:33 -0600

    homework completed

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -31,7 +31,7 @@ const mapStateToProps = function (state) {
     tickets: state.tickets
   };
 };
-export default (Tickets);
+
+const TicketsContainer = connect(mapStateToProps)(Tickets);
+export default TicketsContainer;
 // export default connect(mapStateToProps,null)(Tickets);
-
-
