89047c95b9e88d8043eeac8fe25acaa21e344a64
Matt Cameron <cameron.matt@gmail.com>
2018-05-30 20:18:03 -0500

    converted props to redux

--- src/components/TasksPanel.js
+++ src/components/TasksPanel.js
@@ -1,25 +1,33 @@
 import React from "react";
 import TaskItem from "./TaskItem";
-function TasksPanel(props) {
-  return (  <div className="panel panel-default">
-                <div className="panel-heading">
-                    <h3 className="panel-title"><i className="fa fa-clock-o fa-fw"></i> Tasks Panel</h3>
-                </div>
-                <div className="panel-body">
-                    <div className="list-group">
-                      {
-                        props.tasks.map(function (t,i) {
-                          return <TaskItem key={i} task={t} />;
-                        })
-                      }
-                        
-                    </div>
-                    <div className="text-right">
-                        <a href="#">View All Activity <i className="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                </div>
-          </div>
-        );
-}
+import { connect } from "react-redux";
 
-export default TasksPanel;
+function TasksPanel(props) {
+  return (
+    <div className="panel panel-default">
+      <div className="panel-heading">
+        <h3 className="panel-title">
+          <i className="fa fa-clock-o fa-fw" /> Tasks Panel
+        </h3>
+      </div>
+      <div className="panel-body">
+        <div className="list-group">
+          {props.tasks.map(function(t, i) {
+            return <TaskItem key={i} task={t} />;
+          })}
+        </div>
+        <div className="text-right">
+          <a href="#">
+            View All Activity <i className="fa fa-arrow-circle-right" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+const mapStateToProps = (state) => {
+  return {
+    tasks: state.tasks,
+  };
+};
+export default connect(mapStateToProps, null)(TasksPanel);
