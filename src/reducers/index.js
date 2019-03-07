f2ec6294118bdcbde43e7e71a275b70ce0c367a0
Abby Gottlich <abby.gottlich@gmail.com>
2019-03-07 16:43:18 -0600

    everything working properly

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,48 +1,49 @@
 import { combineReducers } from 'redux'
+import initialState from '../state'
 
-function newComments(state, action) {
+function newComments(state = initialState.newComments, action) {
     if (typeof state === 'undefined') {
         return []
     }
     return state;
 }
 
-function newTasks(state, action) {
+function newTasks(state = initialState.newTasks, action) {
     if (typeof state === 'undefined') {
         return []
     }
     return state;
 }
 
-function newOrders(state, action) {
+function newOrders(state = initialState.newOrders, action) {
     if (typeof state === 'undefined') {
         return []
     }
     return state;
 }
 
-function tickets(state, action) {
+function tickets(state = initialState.tickets, action) {
     if (typeof state === 'undefined') {
         return []
     }
     return state;
 }
 
-function orders(state, action) {
+function orders(state = initialState.orders, action) {
     if (typeof state === 'undefined') {
         return []
     }
     return state;
 }
 
-function tasks(state, action) {
+function tasks(state = initialState.tasks, action) {
     if (typeof state === 'undefined') {
         return []
     }
     return state;
 }
 
-function messages(state, action) {
+function messages(state = initialState.messages, action) {
     if (typeof state === 'undefined') {
         return []
     }
